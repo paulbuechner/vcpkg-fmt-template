8382643c1dd1a5312ccb10eea399adfa7f03cd21
Paul Büchner <45827409+paulbuechner@users.noreply.github.com>
2023-04-23 00:58:34 +0200

    Initial commit

---
+++ src/vcpkg-fmt-template.cc
@@ -0,0 +1,7 @@
+// This file #includes all vcpkg-fmt-template implementation .cc files. The
+// purpose is to allow a user to build vcpkg-fmt-template by compiling this
+// file alone.
+
+// The following lines pull in the real vcpkg-fmt-template*.cc files.
+
+#include "main.cc"
